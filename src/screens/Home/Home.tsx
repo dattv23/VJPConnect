528a09bec308280afdbed79cecf5afab6578b6c2
dattv23 <datvan635@gmail.com>
2024-07-09 10:54:13 +0700

    feat: use webview

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,28 +1,10 @@
-import { Text, View } from 'react-native';
-import { useTranslation } from 'react-i18next';
-
-import { useTheme } from '@/theme';
-import { SafeScreen } from '@/components/template';
+import WebView from 'react-native-webview';
 
 import type { RootScreenProps } from '@/types/navigation';
 
 function Home({ navigation }: RootScreenProps<'Home'>) {
-	const { layout, gutters, fonts } = useTheme();
-	const { t } = useTranslation(['home']);
-
 	return (
-		<SafeScreen>
-			<View
-				style={[
-					layout.flex_1,
-					layout.col,
-					layout.itemsCenter,
-					layout.justifyCenter,
-				]}
-			>
-				<Text style={[fonts.size_16, fonts.red500]}>{t('home:title')}</Text>
-			</View>
-		</SafeScreen>
+		<WebView source={{ uri: 'https://vjp-connect.com/' }} style={{ flex: 1 }} />
 	);
 }
 
